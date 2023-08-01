3db95e7ec8dd6e0cff90a11919c93315d49bb7d2
Kaushal2029 <117632711+Kaushal2029@users.noreply.github.com>
2023-08-01 17:58:47 +0530

    Add files via upload

---
+++ switchcase.php
@@ -0,0 +1,29 @@
+<?php
+ $x;
+ $x=readline("Enter The Number:->");
+ switch($x){
+    case 1:
+        echo "Monday";
+        break;
+    case 2:
+        echo "Tuesday";
+        break;
+    case 3:
+        echo "Wednesday";
+        break;
+    case 4:
+        echo "Thurday";
+        break;
+    case 5:
+        echo "Friday";
+        break;
+    case 6:
+        echo "Saturaday";
+        break;
+    case 7:
+        echo "Sunday";
+        break;
+    default:
+        echo "Invaild Number";}
+ 
+ ?>
